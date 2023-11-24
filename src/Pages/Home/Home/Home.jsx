34cfaa7d513a4a6baf34319119935e96b2150eed
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-25 03:08:53 +0600

    initial setup and rounting

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-center font-bold text-6xl text-emerald-700">This is AssetPulse</h2>
+            <button className="btn btn-outline">Ghost</button>
+
+        </div>
+    );
+};
+
+export default Home;
