4657ad59cbaeed66c0154174b432113f8f90c8a1
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-25 13:09:44 +0600

    adding navbar and footer

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
+import Footer from "../Shared/Footer";
 
 const MainLayouts = () => {
     return (
-        <div>
+        <div >
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,8 +3,6 @@ const Home = () => {
     return (
         <div>
             <h2 className="text-center font-bold text-6xl text-emerald-700">This is AssetPulse</h2>
-            <button className="btn btn-outline">Ghost</button>
-
         </div>
     );
 };
