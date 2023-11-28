6154ffb78bf6a8579d8acdfa61a33b5e940b756e
hmsmiraz <hmsmiraz64729@gmail.com>
2023-11-29 04:06:35 +0600

    added custom list page

--- src/Pages/AdminPages/AllRequests/AllRequests.jsx
+++ src/Pages/AdminPages/AllRequests/AllRequests.jsx
@@ -1,12 +1,132 @@
-
+import { Button } from "@mui/material";
+import useAssetReq from "../../../Hooks/useAssetReq";
+import SharedTitle from "../../../Shared/SharedTitle";
+import { FcApproval, FcDisapprove } from "react-icons/fc";
+import Swal from "sweetalert2";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import { useState } from "react";
 const AllRequests = () => {
-    return (
+  const [assetsReq, refetch] = useAssetReq();
+  const axiosPublic = useAxiosPublic();
+  const [searchByEmail, setSearchByEmail] = useState("");
+
+  const handleSearchByEmail = (event) => {
+    event.preventDefault();
+    setSearchByEmail(event.target.value);
+  };
+  const filterReq = () => {
+    const filterRequests = assetsReq?.filter((item) => {
+      const emailMatch = item?.userEmail
+        .toLowerCase()
+        .includes(searchByEmail.toLowerCase());
+      return emailMatch;
+    });
+    return filterRequests;
+  };
+  const requestData = filterReq();
+  const handleDelete = (item) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosPublic.delete(`/assetReq/${item._id}`);
+        console.log(res.data);
+        if (res.data.deletedCount > 0) {
+          refetch();
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: `${item.name} has been deleted!`,
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      }
+    });
+  };
+  return (
+    <div>
+      <SharedTitle heading={"All Requests"}></SharedTitle>
+      <div>
+        {/* email search */}
         <div>
-            <h2>
-            All Requests
-            </h2>
+          <form
+            onSubmit={handleSearchByEmail}
+            className="flex items-center justify-center"
+          >
+            <div className="form-control">
+              <input
+                type="text"
+                placeholder="Search By email"
+                name="searchByEmail"
+                className="input input-bordered w-24 md:w-48"
+                value={searchByEmail}
+                onChange={handleSearchByEmail}
+              />
+            </div>
+          </form>
         </div>
-    );
+        <div className="my-5">
+          <div className="overflow-x-auto mx-2">
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Asset Name</th>
+                  <th>Type</th>
+                  <th>Email of requester</th>
+                  <th>Name of requester</th>
+                  <th>Request Date</th>
+                  <th>Note</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {requestData.map((item, index) => (
+                  <tr key={item._id}>
+                    <th>{index + 1}</th>
+                    <td>{item.name}</td>
+                    <td>{item.type}</td>
+                    <td>{item.userEmail}</td>
+                    <td>{item.userName}</td>
+                    <td>{item.reqDate}</td>
+                    <td>{item.note}</td>
+                    <td>{item.status}</td>
+                    <td>
+                      <Button className="tooltip" data-tip="Approved">
+                        <FcApproval
+                          className="text-2xl tooltip"
+                          data-tip="Approve"
+                        ></FcApproval>
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        onClick={() => handleDelete(item)}
+                        className="tooltip"
+                        data-tip="Delete"
+                      >
+                        <FcDisapprove className="text-2xl"></FcDisapprove>
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AllRequests;
--- src/Pages/AdminPages/CustomReqList/CustomReqList.jsx
+++ src/Pages/AdminPages/CustomReqList/CustomReqList.jsx
@@ -1,8 +1,95 @@
-
+import useCustomReq from "../../../Hooks/useCustomReq";
+import SharedTitle from "../../../Shared/SharedTitle";
+import { FcApproval, FcDisapprove } from "react-icons/fc";
+import { Button } from "@mui/material";
+import useAxiosPublic from "../../../Hooks/useAxiosPublic";
+import Swal from "sweetalert2";
 const CustomReqList = () => {
+    const[customReq, refetch] = useCustomReq(); 
+  const axiosPublic = useAxiosPublic();
+    const handleDelete = (item) => {
+        Swal.fire({
+          title: "Are you sure?",
+          text: "You won't be able to revert this!",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, delete it!",
+        }).then(async (result) => {
+          if (result.isConfirmed) {
+            const res = await axiosPublic.delete(`/customReq/${item._id}`);
+            console.log(res.data);
+            if (res.data.deletedCount > 0) {
+              refetch();
+              Swal.fire({
+                position: "center",
+                icon: "success",
+                title: `${item.name} has been deleted!`,
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+          }
+        });
+      };
     return (
         <div>
-            <h2>Custom Req List</h2>
+            <SharedTitle heading={"All CUstom requests"}></SharedTitle>
+            <div>
+            <div className="my-5">
+          <div className="overflow-x-auto mx-2">
+            <table className="table w-full">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Asset Name</th>
+                  <th>Price</th>
+                  <th>Type</th>
+                  <th>Image</th>
+                  <th>Reason</th>
+                  <th>additional Info</th>
+                  <th>Date</th>
+                  <th>Action</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {customReq.map((item, index) => (
+                  <tr key={item._id}>
+                    <th>{index + 1}</th>
+                    <td>{item.name}</td>
+                    <td>{item.price}</td>
+                    <td>{item.type}</td>
+                    <td>{item.image}</td>
+                    <td>{item.reason}</td>
+                    <td>{item.additionalInfo}</td>
+                    <td>{item.date}</td>
+                    <td>
+                      <Button className="tooltip" data-tip="Approved">
+                        <FcApproval
+                          className="text-2xl tooltip"
+                          data-tip="Approve"
+                        ></FcApproval>
+                      </Button>
+                    </td>
+                    <td>
+                      <Button
+                        onClick={() => handleDelete(item)}
+                        className="tooltip"
+                        data-tip="Delete"
+                      >
+                        <FcDisapprove className="text-2xl"></FcDisapprove>
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+            </div>
         </div>
     );
 };
